8e3903e3a87dfd8d5a4185ec2d09648db8042b29
Leonardo Albergoni Viana <48710470832@>
2024-09-18 14:42:15 -0300

    CardPerfil

--- projeto-web-metrocamp/src/telas/Clipboard/Clipboard.js
+++ projeto-web-metrocamp/src/telas/Clipboard/Clipboard.js
@@ -1,10 +1,11 @@
 import { CardPrancheta } from "../../componentes/CardPrancheta/CardPrancheta";
 import { Perfil } from "../../componentes/Perfil/Perfil";
+import { CardPerfil } from "../../componentes/CardPerfil/CardPerfil";
 
 export function Clipboard(){
     return(
         <>
-            <Perfil />
+            <CardPerfil />
         </>
     )
 }
