90f42914916d5b2e48c1b3edabf9dfb9554d9910
Joao Belai <joaopbelai@gmail.com>
2024-09-29 19:42:15 -0300

    navbar-arrumada

--- projeto-web-metrocamp/src/componentes/NavBar/NavBar.js
+++ projeto-web-metrocamp/src/componentes/NavBar/NavBar.js
@@ -1,28 +1,68 @@
 import "../NavBar/navbar.css";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaBars, FaTimes, FaClipboardList, FaUser } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
-import { FaClipboardList } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import logo from "../../images/logo.png"
+import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
-
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 export function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 820);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth >= 820);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        
-            <div className="NavBar">
-                <img src={logo} width="100px" alt="" height="100px" />
-                <h5>Precisou de um serviço? <br />Pede pra mim</h5>
-                <div className="search-box">
-                    <button id="search-button"><span className="button-fix"><IoSearch /></span></button>
-                    <input type="search" id="search" placeholder="O que você procura?" />
-                </div>
-                <div className="icons">
-                    <Link to ="/"><div className="home-icon"><FaHome /></div></Link>
-                    <Link to="/Clipboard"><div className="clipboard-icon"><FaClipboardList /></div></Link>
-                    <Link to="/Perfil"><div className="user-icon"><FaUser /></div></Link>
-                </div>
-            </div>
-    )       
+        <div className="NavBar">
+            <img src={logo} width="70px" alt="" height="70px" />
+
+            {isLargeScreen ? (
+                <>
+                    <div className="search-box">
+                        <button id="search-button"><span className="button-fix"><IoSearch /></span></button>
+                        <input type="search" id="search" placeholder="O que você procura?" />
+                    </div>
+                    <div className="icons">
+                        <Link to="/"><div className="home-icon"><FaHome /></div></Link>
+                        <Link to="/Clipboard"><div className="clipboard-icon"><FaClipboardList /></div></Link>
+                        <Link to="/Perfil"><div className="user-icon"><FaUser /></div></Link>
+                    </div>
+                </>
+
+            ) : (
+                <>
+                    <div className="search-box">
+                        <button id="search-button"><span className="button-fix"><IoSearch /></span></button>
+                        <input type="search" id="search" placeholder="O que você procura?" />
+                    </div>
+
+                    <div className="menu-btn" onClick={toggleMenu}>
+                        {menuOpen ? <FaTimes size={20} color="white" /> : <FaBars size={20} color="white" />}
+                    </div>
+
+                    <ul className={`menu ${menuOpen ? 'open' : ''}`}>
+                        <h5 className="menu-title">Precisou de um serviço? <br />Pede pra mim</h5>
+                        <Link to="/"><li><FaHome className="icon" /> Início</li></Link>
+                        <Link to="/Clipboard"><li><FaClipboardList className="icon" /> Tarefas</li></Link>
+                        <Link to="/Perfil"><li><FaUser className="icon" /> Perfil</li></Link>
+                    </ul>
+                </>
+            )
+            }
+        </div>
+    );
 }
