4c82776a86088d91aea52f4b0d7a2fbf58fcb28e
Pedro Henrique <pedrohenrique2937@gmail.com>
2024-10-01 15:17:54 -0300

    CadastroCriandoNoValid

--- projeto-web-metrocamp/src/telas/Cadastro/Cadastro.js
+++ projeto-web-metrocamp/src/telas/Cadastro/Cadastro.js
@@ -1,9 +1,59 @@
 import "./cadastro.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export function Cadastro() {
 
+    const [isLoading, setIsLoading] =useState(false)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Impede o comportamento padrão do submit
+        setIsLoading(true)
 
 
+
+        // Cria o objeto FormData a partir dos campos do formulário
+        const formElement = document.getElementById("CadastroForm");
+        const formData = new FormData(formElement);
+       // const data = Object.fromEntries(formData.entries());
+
+
+      
+        const data = {
+            nome: formData.get("nome"),
+            email: formData.get("email"),
+            senha: formData.get("senha"),
+            telefone: parseInt(formData.get("telefone"), 10), // Converte para int
+        };
+
+        const dados = JSON.stringify(data)
+        
+        try {
+            const response = await fetch("http://localhost:8080/users", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json", // Define o tipo de conteúdo
+                },
+                body: dados, // Enviando os dados do FormData
+            });
+            console.log("Resposta recebida:", response);
+    
+            if (response.ok) {
+                //const data = await response.json();
+                console.log("Usuário criado com sucesso:", data);
+            } else {
+                const errorText = await response.text();
+                console.error("Erro na resposta", response, errorText);
+            }
+        } catch (error) {
+            console.error("Erro de requisição", error);
+           
+        } finally{
+            setTimeout(() => {
+                setIsLoading(false); // Reativa o botão
+              }, 2000); // Tempo em milissegundos
+        }
+    };
+    
 
     return (
         <div className="Cadastro">
@@ -12,25 +62,32 @@ export function Cadastro() {
                 <h5>Cadastro</h5>
             </div>
             
-            <form className="forms" action="https://webhook.site/6eb59883-6c66-4d25-80be-115a37a0ad17" method="get">
+            <form className="forms" onSubmit={handleSubmit} id="CadastroForm">
                 <div className="acertaForms">
 
                     <span className="campos">
-                        <label for="nome">*Nome:</label>
+                        <label>*Nome:</label>
                         <input type="text" name="nome" id="nome" placeholder="Digite seu nome" required></input>
                     </span>
 
                     <span className="campos">
-                        <label for="email">*Email:</label>
+                        <label>*Email:</label>
                         <input type="text" name="email" id="email" placeholder="Digite seu Email" required></input>
                     </span>
 
                     <span className="campos">
-                        <label for="password">*Senha:</label>
-                        <input type="password" name="password" id="password" placeholder="Digite sua senha" required></input>
+                        <label>*Senha:</label>
+                        <input type="password" name="senha" id="senha" placeholder="Digite sua senha" required></input>
                     </span>
 
-                    <button type="submit" className="botao-forms">Cadastrar</button>
+                    <span className="campos">
+                        <label>*Telefone:</label>
+                        <input type="text" name="telefone" id="telefone" placeholder="Digite seu número" required></input>
+                    </span>
+
+                    <button type="submit" className="botao-forms"  style={{
+                        backgroundColor: isLoading ? '#ccc' : '#007BFF', 
+                        cursor: isLoading ? 'not-allowed' : 'pointer', }}>Cadastrar</button>
 
 
 
